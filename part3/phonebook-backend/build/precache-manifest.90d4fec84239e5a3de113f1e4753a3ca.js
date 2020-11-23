b920ab0853525c53de3d4a38c52e14ebabf4b543
Navid Farhadi <nfarhadi@kth.se>
2020-11-23 20:17:18 +0100

    adding part 3 code up to 3.18

---
+++ part3/phonebook-backend/build/precache-manifest.90d4fec84239e5a3de113f1e4753a3ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7c67a5fab60835d06de2cfa412c0c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db4a0ea7430c84f890ed",
+    "url": "/static/css/main.98ad490f.chunk.css"
+  },
+  {
+    "revision": "045886ead075971730b6",
+    "url": "/static/js/2.68633c19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68633c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db4a0ea7430c84f890ed",
+    "url": "/static/js/main.006d3408.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,107 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const Person = require('./models/person')
+
+morgan.token('reqBody', (request, response) => {
+  if (request.method === "POST")
+    return JSON.stringify(request.body)
+})
+
+app.use(express.static('build'))
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
+
+app.get('/info', (request, response) => {
+  Person.countDocuments({})
+    .then(count => {
+      response.send(`Phonebook has info for ${count} people <br><br> ${Date()}`)
+    })
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if(person) {
+      response.json(person)
+    }
+    else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response) => {
+  if (request.body.name === undefined) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  if (request.body.number === undefined) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const person = new Person({
+    name: request.body.name,
+    number: request.body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const person = {
+    name: request.body.name,
+    number: request.body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
