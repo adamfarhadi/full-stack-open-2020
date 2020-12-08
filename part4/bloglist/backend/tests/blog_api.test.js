71910e8c00e2a9c95b7450802c01e436a1ebf37b
Navid Farhadi <nfarhadi@kth.se>
2020-12-08 12:33:37 +0100

    4.8 done

---
+++ part4/bloglist/backend/tests/blog_api.test.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+test('correct amount of blogs are returned in JSON format', async () => {
+  const response = await api.get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
