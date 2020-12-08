0805fbe6c7cc26698714060ac07ccb88f3d5e47e
Adam Farhadi <adam@adamfarhadi.com>
2020-12-08 14:25:11 +0100

    4.9 and 4.10 done

--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { response } = require('../app')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -6,14 +7,10 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
--- part4/bloglist/backend/tests/blog_api.test.js
+++ part4/bloglist/backend/tests/blog_api.test.js
@@ -23,6 +23,31 @@ test('correct amount of blogs are returned in JSON format', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('unique identifier is named id', async () => {
+  const response = await api.get('/api/blogs/')
+  response.body.map((blog) => expect(blog).toHaveProperty('id'))
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'How to make fried rice',
+    author: 'Dr. Fried McRice',
+    url: 'http://fried.rice',
+    likes: '749'
+  }
+
+  await api.post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsinDB()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(blogs => blogs.title)
+  expect(titles).toContain(newBlog.title)
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
